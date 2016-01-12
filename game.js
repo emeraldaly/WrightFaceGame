1a51d0fdaf1828dd5c55e916f6b05037d679cbb3
Neelesh <ntekal@gmail.com>
2016-01-12 16:08:19 -0500

    reformatted to include img-rounded, all of jQuery functionality and working modals

--- game.js
+++ game.js
@@ -1,13 +1,37 @@
-var counter = 0;
+document.ready(function(){
 
-function countup() {
-  counter = counter + 1;
-}
+  var counter = 0;
+  $("td img").addClass("img-responsive");
+  $(".img").attr("disabled", true);
+
+  $(".img-responsive").on("click", function(){
+    if($(this).attr("disabled")){
+      return;
+    } else{
+      $(this).removeClass("on").addClass("off").attr("disabled", true);
+      countup();
+    }
+  });
+
+  function countup() {
+    counter = counter + 1;
+  }
 
 
-function countdown() {
-  setTimeout(function countandgive() { 
-      alert("You clicked " +counter+ " Comedians");
-     
-     }, 20000);
-}
+  $('#startButton').on('click', function () {
+    counter = 0;
+    $(".img-responsive").attr("disabled", false).removeClass("off").addClass("on");
+    setTimeout(function countAndGive(){
+      $(".modal-body").empty();
+      $(".modal-body").append("<p>You clicked " + scoreCounter + "comedians.</p>");
+      $("#resultModal").modal("show");
+    }, 20000);
+  });
+
+});
+
+
+
+
+
+
